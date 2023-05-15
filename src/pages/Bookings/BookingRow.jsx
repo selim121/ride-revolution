6dcb7f6026664f04ec34b0f43f007627335e7d11
selim121 <selimkhan.bdkhan@gmail.com>
2023-05-15 23:30:16 +0600

    bookings update added

--- src/pages/Bookings/BookingRow.jsx
+++ src/pages/Bookings/BookingRow.jsx
@@ -1,26 +1,29 @@
 
-const BookingRow = ({booking}) => {
+const BookingRow = ({ booking, handleDelete, handleConfirm }) => {
 
-    const {customerName, email, date, img, title, price} = booking;
+    const { _id, customerName, email, date, img, title, price, status } = booking;
+
+
 
     return (
         <>
             <tr>
                 <th>
-                    <label>
-                        <input type="checkbox" className="checkbox" />
-                    </label>
+                    <button onClick={() => handleDelete(_id)} className="btn btn-sm btn-circle">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
+                    </button>
+
                 </th>
                 <td>
-                        <div className="avatar">
-                            <div className="rounded w-24 h-24">
-                                <img src={img} alt="Avatar Tailwind CSS Component" />
-                            </div>
+                    <div className="avatar">
+                        <div className="rounded w-24 h-24">
+                            <img src={img} alt="Avatar Tailwind CSS Component" />
                         </div>
-                        <div>
-                            <div className="font-bold">{title}</div>
-                            <div className="text-sm opacity-50">{price}</div>
-                        </div>
+                    </div>
+                    <div>
+                        <div className="font-bold">{title}</div>
+                        <div className="text-sm opacity-50">$ {price}</div>
+                    </div>
                 </td>
                 <td>
                     {customerName}
@@ -28,7 +31,10 @@ const BookingRow = ({booking}) => {
                 <td>{email}</td>
                 <td>{date}</td>
                 <th>
-                    <button className="btn btn-ghost btn-xs">Pending</button>
+                    {
+                        status === 'confirm' ? <span className="font-bold text-primary">Confirmed</span> :
+                        <button onClick={() => handleConfirm(_id)} className="btn btn-ghost text-warning btn-xs">Please confirm</button>
+                    }
                 </th>
             </tr>
         </>
--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -14,7 +14,52 @@ const Bookings = () => {
             .then(data => {
                 setBookings(data);
             })
-    }, [])
+    }, [url])
+
+    const handleDelete = id => {
+        const preceed = confirm('Are you sure, you want to delete?');
+        if(preceed){
+            fetch(`http://localhost:4000/bookings/${id}`, {
+                method: 'DELETE'
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.deletedCount > 0){
+                    alert('Deleted successfully!');
+                    const remaining = bookings.filter(booking => booking._id !== id);
+                    setBookings(remaining);
+                }
+            })
+        }
+    }
+
+    const handleConfirm =id => {
+        const preceed = confirm('Are you sure?');
+        if(preceed) {
+            fetch(`http://localhost:4000/bookings/${id}`, {
+                method: 'PATCH',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify({status: 'confirm'})
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.modifiedCount>0){
+                    //update state
+                    const remaining = bookings.filter(booking => booking._id !== id);
+                    const updated = bookings.find(booking => booking._id !== id);
+                    updated.status = 'confirm'
+                    const newBookings = [updated, ...remaining];
+                    setBookings(newBookings);
+                    
+                }
+
+            })
+        }
+    }
 
     return (
         <div className="my-12">
@@ -27,7 +72,7 @@ const Bookings = () => {
                             <th>Name</th>
                             <th>Email</th>
                             <th>Price</th>
-                            <th></th>
+                            <th>Status</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -35,6 +80,8 @@ const Bookings = () => {
                             bookings.map(booking => <BookingRow
                                 key={booking._id}
                                 booking={booking}
+                                handleDelete={handleDelete}
+                                handleConfirm={handleConfirm}
                             ></BookingRow>)
                         }
                     </tbody>
