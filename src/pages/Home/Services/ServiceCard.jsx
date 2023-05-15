a021a2de81037519e17be18d62fdf7dfc5db6abc
selim121 <selimkhan.bdkhan@gmail.com>
2023-05-15 23:44:07 +0600

    user handle

--- src/pages/Home/Services/ServiceCard.jsx
+++ src/pages/Home/Services/ServiceCard.jsx
@@ -1,9 +1,15 @@
 /* eslint-disable react/prop-types */
 
+import { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../../../providers/AuthProvider";
+
 
 const ServiceCard = ({ service }) => {
-    const {_id, title, img, price } = service;
+
+    const { user } = useContext(AuthContext);
+    const { _id, title, img, price } = service;
+
     return (
         <div className="card w-96 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
@@ -14,7 +20,7 @@ const ServiceCard = ({ service }) => {
                 <div className="flex justify-between items-center">
                     <p className="text-xl text-orange-500">Price: ${price}</p>
                     <div className="card-actions">
-                        <Link to={`/book/${_id}`}><button className="btn btn-primary">Book Now</button></Link>
+                        <Link to={user? `/book/${_id}` : '/login'}><button className="btn btn-primary">Book Now</button></Link>
                     </div>
                 </div>
             </div>
